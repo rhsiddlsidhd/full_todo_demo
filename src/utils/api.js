1c086116b613f0fc3c2745ebc5894233e78eeb43
Young <rhsiddlsidhd@nate.com>
2024-06-23 09:12:59 +0900

    error handle

--- src/utils/api.js
+++ src/utils/api.js
@@ -20,15 +20,37 @@ api.interceptors.request.use(
   }
 );
 
+// api.interceptors.response.use(
+//   (response) => {
+//     console.log("Response:", response);
+//     return response;
+//   },
+//   function (error) {
+//     error = error.response.data;
+//     console.log("RESPONSE ERROR", error);
+//     return Promise.reject(error);
+//   }
+// );
+
 api.interceptors.response.use(
   (response) => {
     console.log("Response:", response);
     return response;
   },
-  function (error) {
-    error = error.response.data;
-    console.log("RESPONSE ERROR", error.message);
-    return Promise.reject(error);
+  (error) => {
+    if (error.response) {
+      // 서버가 응답을 보낸 경우 (4xx, 5xx)
+      console.log("RESPONSE ERROR", error.response.data);
+      return Promise.reject(error.response.data);
+    } else if (error.request) {
+      // 요청이 만들어졌지만 서버로부터 응답이 없는 경우
+      console.log("NO RESPONSE RECEIVED", error.request);
+      return Promise.reject({ message: "No response received from server" });
+    } else {
+      // 요청 설정 중에 에러가 발생한 경우
+      console.log("REQUEST SETUP ERROR", error.message);
+      return Promise.reject({ message: error.message });
+    }
   }
 );
 
